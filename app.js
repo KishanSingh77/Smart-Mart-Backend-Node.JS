9a6bed4da8e96e2b3554cb2024a99eca943b6aea
Kishan S <42445381+KishanSingh77@users.noreply.github.com>
2019-11-12 14:34:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const vendorRoutes = require("./api/routes/vendor");
 
 mongoose.connect(
   "mongodb://admin:" +
-    process.env.MONGO_ATLAS_PW +
+    "admin99" +
     "@cluster0-shard-00-00-nk8pq.mongodb.net:27017,cluster0-shard-00-01-nk8pq.mongodb.net:27017,cluster0-shard-00-02-nk8pq.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
 );
 const db = mongoose.connection;
